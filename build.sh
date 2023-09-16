f2540ee1c4997e06b1e718fff5024fb02f0098b4
Exotic <exotic@localhost>
2023-09-16 10:29:40 +0200

    Use the new -Zlocation-detail option in build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cargo update
-RUSTFLAGS="-C target-cpu=x86-64-v3" cargo +nightly build -Z build-std=core,std,panic_abort,alloc,proc_macro -Z build-std-features=panic_immediate_abort --target x86_64-unknown-linux-gnu --release
+RUSTFLAGS="-C target-cpu=x86-64-v3 -Zlocation-detail=none" cargo +nightly build -Z build-std=core,std,panic_abort,alloc,proc_macro -Z build-std-features=panic_immediate_abort --target x86_64-unknown-linux-gnu --release
