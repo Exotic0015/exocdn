b0072acf3bb8e2828564cfc4a4e4ffabe56ecb6e
Exotic <exotic@localhost>
2023-10-16 18:30:28 +0200

    Use upx in release build

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cargo update
 RUSTFLAGS="-C target-cpu=x86-64-v3 -Zlocation-detail=none" cargo +nightly build -Z build-std=core,std,panic_abort,alloc,proc_macro -Z build-std-features=panic_immediate_abort --target x86_64-unknown-linux-gnu --release
+upx --best --lzma target/x86_64-unknown-linux-gnu/release/exocdn
