fa41cf5652b5b90698e6bdf33291b018589fe619
Exotic <26276775+Exotic0015@users.noreply.github.com>
2023-10-30 00:27:24 +0100

    Enable Transparent Huge Pages

--- build-linux.sh
+++ build-linux.sh
@@ -5,6 +5,7 @@ TARGET_CPU=${user_input:-x86-64-v3}
 echo "Updating dependencies"
 cargo update
 
+export MALLOC_CONF="thp:always,metadata_thp:always" # Enable Transparent Huge Pages
 echo "Building for target CPU: $TARGET_CPU"
 RUSTFLAGS="-C target-cpu=$TARGET_CPU -Zlocation-detail=none" cargo +nightly build -Z build-std=core,std,panic_abort,alloc,proc_macro -Z build-std-features=panic_immediate_abort --target x86_64-unknown-linux-gnu --release
 
