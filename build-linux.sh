78435366c98201ef51f66a26686981293de839b8
Exotic <26276775+Exotic0015@users.noreply.github.com>
2026-01-14 15:17:17 +0100

    Adjust for nightly rust changes

--- build-linux.sh
+++ build-linux.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -e
+
 read -r -p "Enter the target CPU (default: x86-64-v3): " user_input
 TARGET_CPU=${user_input:-x86-64-v3}
 
@@ -19,7 +21,7 @@ cargo update
 
 export MALLOC_CONF="thp:always,metadata_thp:always" # Enable Transparent Huge Pages
 echo "Building for target CPU: $TARGET_CPU"
-CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER="clang" RUSTFLAGS="-Clink-arg=-fuse-ld=lld -Clink-arg=-Wl,--icf=all -C target-cpu=$TARGET_CPU -Zlocation-detail=none -Zshare-generics=y -Zthreads=0" cargo +nightly build -Z build-std=core,std,panic_abort,alloc,proc_macro -Z build-std-features=panic-unwind,panic_immediate_abort ${COMPRESSION:""} --target x86_64-unknown-linux-gnu --release
+CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER="clang" RUSTFLAGS="-Clink-arg=-fuse-ld=lld -Clink-arg=-Wl,--icf=all -C target-cpu=$TARGET_CPU -Zlocation-detail=none -Zshare-generics=y -Zthreads=0" cargo +nightly build -Z build-std=core,std,panic_abort,alloc,proc_macro -Z build-std-features=panic-unwind ${COMPRESSION:""} --target x86_64-unknown-linux-gnu --release
 
 echo "Packing the binary"
 upx --best --lzma target/x86_64-unknown-linux-gnu/release/exocdn
